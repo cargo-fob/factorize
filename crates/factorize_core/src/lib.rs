eac58451c57b4ec267613f032874c2ce3d227d99
jaehafe <wogkdkrm112@gmail.com>
2026-04-04 21:56:09 +0900

    feat: factorize_core

--- crates/factorize_core/src/lib.rs
+++ crates/factorize_core/src/lib.rs
@@ -1,6 +1,30 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+struct CompilerState(Arc<AtomicBool>);
+
+impl CompilerState {
+    fn init() -> Self {
+        Self(Arc::new(AtomicBool::new(false)))
+    }
+}
+
+impl CompilerState {
+    fn running(&self) -> bool {
+        self.0.load(Ordering::Relaxed)
+    }
+
+    fn enter(&self) -> CompilerStateGuard {
+        self.0.store(true, Ordering::Relaxed);
+        CompilerStateGuard(self.0.clone())
+    }
+}
+
+struct CompilerStateGuard(Arc<AtomicBool>);
 
 #[cfg(test)]
 mod tests {
